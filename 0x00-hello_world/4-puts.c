2460901bfdee82cc37cc2e3db6a12687f0d814c1
Ola <ilawal667@gmail.com>
2022-10-13 16:44:59 -0700

    put

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
-
 /**
- *main - Entry point
+ * main - Entry point
  *
  * return: Value 0
-*/
-
+ */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle,");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
