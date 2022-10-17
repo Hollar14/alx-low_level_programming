5a5202fd0d7bae2284b7ce13ff00f68a24d766be
Ola <ilawal667@gmail.com>
2022-10-17 12:52:28 -0700

    base16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+* main - print digit of base 16
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int num;
+	char lc;
+
+	for (num = 0; num < 10; num++)
+	putchar(num);
+
+	for (lc = 'a'; lc < 'f'; lc++)
+	putchar(lc);
+
+	putchar('\n');
+
+	return (0);
+}
