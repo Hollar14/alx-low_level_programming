e781785f80ee094d7a911d05cc95989e11cf426b
Ola <ilawal667@gmail.com>
2022-10-17 12:47:14 -0700

    tebahpla

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+* main - print lowercase alp in reverse.
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	char al;
+
+	for (al = 'z'; al >= 'a'; al--)
+
+	putchar(al);
+
+	putchar('\n');
+
+	return (0);
+}
