c9bfcf9eaa380072d2643cd6e6e1ee087ae4797f
Ola <ilawal667@gmail.com>
2022-10-18 13:56:15 -0700

    main

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,6 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+int _putchar(char c)
 void print_alphabet(void)
 void print_alphabet_x10(void)
 int _islower(int c);
@@ -12,6 +13,5 @@ void jack_table(void)
 void time_table(void)
 int add(int, int);
 void print_to_98(int n);
-int_ putchar(char c);
 
 #endif
