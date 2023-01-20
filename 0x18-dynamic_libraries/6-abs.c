0189b102cce6a2f645ef7cf781313ed0dbf55869
hollar14 <i_lawal@outlook.com>
2023-01-20 00:20:01 -0800

    dynamic lib

---
+++ 0x18-dynamic_libraries/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+* _abs - print the asolute value of an int
+* @n: The interger to be checked
+*
+* Return: the abs of the int
+*/
+
+int _abs(int n)
+{
+	if (n >= 0)
+		return (n);
+	else
+		return (-n);
+}
